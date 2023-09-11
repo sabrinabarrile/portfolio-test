58daa724fa6a5319b27d4d86952401b94f73e0a1
sabrinabarrile <124946487+sabrinabarrile@users.noreply.github.com>
2023-09-11 11:10:46 -0400

    Removal of errors

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -3,9 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import LaptopImg from "../../assets/home-main.svg";
 import Tilt from "react-parallax-tilt";
 import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
+  AiFillGithub
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
